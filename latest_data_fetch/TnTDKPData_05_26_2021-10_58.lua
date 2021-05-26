a4dd594ff5710e27043a303adcbc2cce45a53828
Matt Caruano <mjcengineer89@yahoo.com>
2021-05-26 10:59:59 -0700

    Updating with initial data for TBC. This is effectively clearing out all data

---
+++ latest_data_fetch/TnTDKPData_05_26_2021-10_58.lua
@@ -0,0 +1,28 @@
+T4_PRIORITY_DKP_TABLE = {
+   ["Akaran"] = 0,
+}
+T4_LOTTERY_DKP_TABLE = {
+   ["Akaran"] = 0,
+}
+T5_PRIORITY_DKP_TABLE = {
+   ["Akaran"] = 0,
+}
+T5_LOTTERY_DKP_TABLE = {
+   ["Akaran"] = 0,
+}
+T6_PRIORITY_DKP_TABLE = {
+   ["Akaran"] = 0,
+}
+T6_LOTTERY_DKP_TABLE = {
+   ["Akaran"] = 0,
+}
+T6PT5_PRIORITY_DKP_TABLE = {
+   ["Akaran"] = 0,
+}
+T6PT5_LOTTERY_DKP_TABLE = {
+   ["Akaran"] = 0,
+}
+PLAYER_PRIORITY_REGISTRY = {
+}
+PLAYER_LOTTERY_REGISTRY = {
+}

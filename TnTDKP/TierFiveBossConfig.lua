04e1cd30fcc1ef0d06fda049ce5818984a801538
Matt Caruano <mjcengineer89@yahoo.com>
2021-07-28 17:53:51 -0700

    Set initial DKP values for T5 bosses

--- TnTDKP/TierFiveBossConfig.lua
+++ TnTDKP/TierFiveBossConfig.lua
@@ -1,6 +1,42 @@
 -- The configuration which hosts boss-specific configuration variables.
 tierFiveBossConfig = {	
-	["TODO"] = {
+	["Hydross the Unstable"] = {
 		["KillDKPAward"] = 100,
+	},
+
+	["The Lurker Below"] = {
+		["KillDKPAward"] = 100,
+	},
+
+	["Leotheras the Blind"] = {
+		["KillDKPAward"] = 150,
+	},
+
+	["Kathom-Lord Karathress"] = {
+		["KillDKPAward"] = 100,
+	},
+
+	["Morogrim Tidewalker"] = {
+		["KillDKPAward"] = 100,
+	},
+
+	["Lady Vashj"] = {
+		["KillDKPAward"] = 180,
+	},
+
+	["Al'ar"] = {
+		["KillDKPAward"] = 100,
+	},
+
+	["Void Reaver"] = {
+		["KillDKPAward"] = 100,
+	},
+
+	["High Astromancer Solarian"] = {
+		["KillDKPAward"] = 100,
+	},
+
+	["Kael'thas Sunstrider"] = {
+		["KillDKPAward"] = 150,
 	},
 }

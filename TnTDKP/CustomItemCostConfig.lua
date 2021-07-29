63724c395bdf899e6b3538d6fe2250dc4d91998d
Matt Caruano <mjcengineer89@yahoo.com>
2021-07-28 17:45:25 -0700

    Added all T5 loot. Set the BoE patterns to be 50% cost

--- TnTDKP/CustomItemCostConfig.lua
+++ TnTDKP/CustomItemCostConfig.lua
@@ -1,4 +1,14 @@
 customItemCostConfig = {
-    -- Pit Lord's Satchel --
+    -- Pit Lord's Satchel
 	[34845] = 2000,
+
+    -- SSC/TK BoE Patterns
+    [30307] = 1000, -- Pattern: Boots of the Crimson Hawk
+    [30308] = 1000, -- Pattern: Hurricane Boots
+    [30282] = 1000, -- Pattern: Boots of Blasting
+    [30283] = 1000, -- Pattern: Boots of the Long Road
+    [30305] = 1000, -- Pattern: Boots of Natural Grace
+    [30306] = 1000, -- Pattern: Boots of Utter Darkness
+    [30323] = 1000, -- Plans: Boots of the Protector
+    [30324] = 1000, -- Plans: Red Havoc Boots
 }
--- TnTDKP/TierFiveLoot.lua
+++ TnTDKP/TierFiveLoot.lua
@@ -1,5 +1,171 @@
--- This table simply contains the itemIDs of all loot from T4 content (Magtheridon/Gruul's Lair)
+-- This table simply contains the itemIDs of all loot from T5 content (SSC/TK)
 tierFiveLoot = {
-    -- Trash drops
-    [1] = 99999, -- TODO
+    -- SSC Trash drops
+    [1] = 30025, -- Serpentshrine Shuriken
+    [2] = 30021, -- Wildfury Greatstaff
+    [3] = 30027, -- Boots of Courage Unending
+    [4] = 30022, -- Pendant of the Perilous
+    [5] = 30620, -- Spyglass of the Hidden Fleet
+    [6] = 30023, -- Totem of the Maelstrom
+
+    -- SSC/TK BoE Patterns (which create BoP items)
+    [7] = 30307, -- Pattern: Boots of the Crimson Hawk
+    [8] = 30308, -- Pattern: Hurricane Boots
+    [9] = 30282, -- Pattern: Boots of Blasting
+    [10] = 30283, -- Pattern: Boots of the Long Road
+    [11] = 30305, -- Pattern: Boots of Natural Grace
+    [12] = 30306, -- Pattern: Boots of Utter Darkness
+    [13] = 30323, -- Plans: Boots of the Protector
+    [14] = 30324, -- Plans: Red Havoc Boots
+
+    -- Hydross the Unstable
+    [15] = 30048, -- Brighthelm of Justice
+    [16] = 30047, -- Blackfathom Warbands
+    [17] = 30052, -- Ring of Lethality
+    [18] = 30629, -- Scarab of Displacement
+    [19] = 30049, -- Fathomstone
+    [20] = 33055, -- Band of Vile Aggression
+    [21] = 30050, -- Boots of the Shifting Nightmare
+    [22] = 30664, -- Living Root of the Wildheart
+    [23] = 30053, -- Pauldrons of the Wardancer
+    [24] = 30054, -- Ranger-General's Chestguard
+    [25] = 30056, -- Robe of Hateful Echoes
+    [26] = 30055, -- Shoulderpads of the Stranger
+    [27] = 32516, -- Wraps of Purification
+    [28] = 30051, -- Idol of the Crescent Goddess
+
+    -- The Lurker Below
+    [29] = 30059, -- Choker of Animalistic Fury
+    [30] = 30058, -- Mallet of the Tides
+    [31] = 30057, -- Bracers of Eradication
+    [32] = 30065, -- Glowing Breastplate of Truth
+    [33] = 30066, -- Tempest-Strider Boots
+    [34] = 30061, -- Ancestral Ring of Conquest
+    [35] = 30060, -- Boots of Effortless Striking
+    [36] = 30062, -- Grove-Bands of Remulos
+    [37] = 30063, -- Libram of Absolute Truth
+    [38] = 30064, -- Cord of Screaming Terrors
+    [39] = 30665, -- Earring of Soulful Meditation
+    [40] = 30067, -- Velvet Boots of the Guardian
+    [41] = 33054, -- The Seal of Danzalar
+
+    -- Leotheras the Blind
+    [42] = 30240, -- Gloves of the Vanquished Defender
+    [43] = 30239, -- Gloves of the Vanquished Champion
+    [44] = 30241, -- Gloves of the Vanquished Hero
+    [45] = 30627, -- Tsunami Talisman
+    [46] = 30097, -- Coral-Barbed Shoulderpads
+    [47] = 30091, -- True-Aim Stalker Bands
+    [48] = 30096, -- Girdle of the Invulnerable
+    [49] = 30095, -- Fang of the leviathan
+    [50] = 30092, -- Orca-Hide Boots
+
+    -- Fathom-Lord Karathress
+    [51] = 30247, -- Leggings of the Vanquished Hero
+    [52] = 30245, -- Leggings of the Vanquished Champion
+    [53] = 30246, -- Leggings of the Vanquished Defender
+    [54] = 30099, -- Frayed Tether of the Drowned
+    [55] = 30101, -- Bloodsea Brigand's Vest
+    [56] = 30090, -- World Breaker
+    [57] = 30100, -- Soul-Strider Boots
+    [58] = 30626, -- Sextant of Unstable Currents
+    [59] = 30663, -- Fathom-Brooch of the Tidewalker
+
+    -- Morogrim Tidewalker
+    [60] = 30098, -- Razor-Scale Battlecloak
+    [61] = 30720, -- Serpent-Coil Braid
+    [62] = 30082, -- Talon of Azshara
+    [63] = 33058, -- Band of the Vigilant
+    [64] = 30084, -- Pauldrons of the Argent Sentinel
+    [65] = 30085, -- Mantle of the Tireless Tracker
+    [66] = 30081, -- Warboots of Obliteration
+    [67] = 30083, -- Ring of Sundered Souls
+    [68] = 30068, -- Girdle of the Tidal Call
+    [69] = 30079, -- Illidari Shoulderpads
+    [70] = 30080, -- Luminescent Rod of the Naaru
+    [71] = 30075, -- Gnarled Chestpiece of the Ancients
+    [72] = 30008, -- Pendant of the Lost Ages
+
+    -- Lady Vashj
+    [73] = 30244, -- Helm of the Vanquished Hero
+    [74] = 30242, -- Helm of the Vanquished Champion
+    [75] = 30243, -- Helm of the Vanquished Defender
+    [76] = 30110, -- Coral Band of the Revived
+    [77] = 30106, -- Belt of One-Hundred Deaths
+    [78] = 30112, -- Glorious Gauntlets of Crestfall
+    [79] = 30111, -- Runetotem's Mantle
+    [80] = 30104, -- Cobra-Lash Boots
+    [81] = 30621, -- Prism of Inner Calm
+    [82] = 30109, -- Ring of Endless Coils
+    [83] = 30102, -- Krakken-Heart Breastplate
+    [84] = 30105, -- Serpent Spine Longbow
+    [85] = 30107, -- Vestments of the Sea-Witch
+    [86] = 30108, -- Lightfathom Scepter
+    [87] = 30103, -- Fang of Vashj
+
+    -- TK Trash drops
+    [88] = 30026, -- Bands of the Celestial Archer
+    [89] = 30024, -- Mantle of the Elven Kings
+    [90] = 30030, -- Girdle of Fallen Stars
+    [91] = 30020, -- Fire-Cord of the Magus
+
+    -- Void Reaver
+    [92] = 30248, -- Pauldrons of the Vanquished Champion
+    [93] = 30249, -- Pauldrons of the Vanquished Defender
+    [94] = 30250, -- Pauldrons of the Vanquished Hero
+    [95] = 29985, -- Void Reaver Greaves
+    [96] = 32515, -- Wristguards of Determination
+    [97] = 29986, -- Cowl of the Grand Engineer
+    [98] = 30619, -- Fel Reaver's Piston
+    [99] = 29983, -- Fel-Steel Warhelm
+    [100] = 29984, -- Girdle of Zaetar
+    [101] = 30450, -- Warp-Spring Coil
+
+    -- Al'ar
+    [102] = 29924, -- Netherbane
+    [103] = 29923, -- Talisman of the Sun King
+    [104] = 29949, -- Arcanite Steam-Pistol
+    [105] = 29922, -- Band of Al'ar
+    [106] = 29947, -- Gloves of the Searing Grip
+    [107] = 30448, -- Talon of Al'ar
+    [108] = 29921, -- Fire Crest Breastplate
+    [109] = 29918, -- Mindstorm Wristbands
+    [110] = 29925, -- Phoenix-Wing Cloak
+    [111] = 29920, -- Phoenix-Ring of Rebirth
+    [112] = 30447, -- Tome of Fiery Redemption
+    [113] = 29948, -- Claw of the Phoenix
+    [114] = 32944, -- Talon of the Phoenix
+
+    -- High Astromancer Solarian
+    [115] = 29981, -- Ethereum Life-Staff
+    [116] = 30449, -- Void Star Talisman
+    [117] = 29950, -- Greaves of the Bloodwarder
+    [118] = 30446, -- Solarian's Sapphire
+    [119] = 29966, -- Vambraces of Ending
+    [120] = 29965, -- Girdle of the Righteous Path
+    [121] = 29962, -- Heartrazor
+    [122] = 29951, -- Star-Strider Boots
+    [123] = 29977, -- Star-Soul Breeches
+    [124] = 29972, -- Trousers of the Astromancer
+    [125] = 29976, -- Worldstorm Gauntlets
+    [126] = 32267, -- Boots of the Resilient
+    [127] = 29982, -- Wand of the Forgotten Star
+
+    -- Kael'thas Sunstrider
+    [128] = 32405, -- Verdant Sphere
+    [129] = 30238, -- Chestguard of the Vanquished Hero
+    [130] = 30236, -- Chestguard of the Vanquished Champion
+    [131] = 30237, -- Chestguard of the Vanquished Defender
+    [132] = 29987, -- Gauntlets of the Sun King
+    [133] = 29994, -- Thalassian Wildercloak
+    [134] = 29997, -- Band of the Ranger-General
+    [135] = 29990, -- Crown of the Sun
+    [136] = 29996, -- Rod of the Sun King
+    [137] = 29998, -- Royal Gauntlets of Silvermoon
+    [138] = 29988, -- The Nexus Key
+    [139] = 29993, -- Twinblade of the Phoenix
+    [140] = 29992, -- Royal Cloak of the Sunstriders
+    [141] = 29989, -- Sunshower Light Cloak
+    [142] = 29995, -- Leggings of Murderous Intent
+    [143] = 29991, -- Sunhawk Leggings
 }

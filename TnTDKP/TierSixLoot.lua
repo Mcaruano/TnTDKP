e781ae791aea8aff138110f03465ec19c8778fbf
Matt Caruano <mjcengineer89@yahoo.com>
2021-12-16 21:45:53 -0800

    Started adding T6 loot. Not done yet

--- TnTDKP/TierSixLoot.lua
+++ TnTDKP/TierSixLoot.lua
@@ -1,5 +1,143 @@
 -- This table simply contains the itemIDs of all loot from T6 content (BT/Hyjal)
 tierSixLoot = {
-    -- Trash drops
+    -- BT Trash drops
+    [1] = 32528, -- Blessed Band of Karabor
+    [1] = 34011, -- Illidari Runeshield
+    [1] = 32526, -- Band of Devastation
+    [1] = 32527, -- Ring of Ancient Knowledge
+    [1] = 99999, -- Shroud of the Final Stand
+    [1] = 99999, -- Swiftsteel Bludgeon
+    [1] = 32606, -- Girdle of the Lightbearer
+
+    [1] = 32749, -- Pattern: Shoulders of Lightning Reflexes
+    [1] = 32753, -- Pattern: Swiftheal Mantle
+    [1] = 32737, -- Plans: Swiftsteel Shoulders
+    [1] = 32754, -- Pattern: Bracers of Nimble Thought
+    [1] = 32744, -- Pattern: Bracers of Renewed Life
+    [1] = 32750, -- Pattern: Living Earth Bindings
+    [1] = 32747, -- Pattern: Swiftstrike Shoulders
+
+    -- BT Shared Boss Patterns
+    [1] = 32738, -- Plans: Dawnsteel Bracers
+    [1] = 32739, -- Plans: Dawnsteel Shoulders
+    [1] = 32745, -- Pattern: Shoulderpads of Renewed Life
+    [1] = 32751, -- Pattern: Living Earth Shoulders
+    [1] = 32755, -- Pattern: Mantle of Nimble Thought
+    [1] = 32752, -- Pattern: Swiftheal Wraps
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+
+
+    -- High Warlord Naj'entus
+    [1] = 32247, -- Ring of Captured Storms
+    [1] = 32377, -- Mantle of Darkness
+    [1] = 32238, -- Ring of Calming Waves
+    [1] = 32234, -- Fists of Mukoa
+    [1] = 32248, -- Halberd of Desolation
+    [1] = 32239, -- Slippers of the Seacaller
+    [1] = 32237, -- The Maelstrom's Fury
+    [1] = 32232, -- Eternium Shell Bracers
+    [1] = 32240, -- Guise of the Tidal Lurker
+    [1] = 32236, -- Rising Tide
+    [1] = 32243, -- Pearl Inlaid Boots
+    [1] = 32241, -- Helm of Soothing Currents
+    [1] = 32242, -- Boots of Oceanic Fury
+    [1] = 32245, -- Tide-stomper's Greaves
+
+    -- Supremus
+    [1] = 32261, -- Band of the Abyssal Lord
+    [1] = 32257, -- Idol of the White Stag
+    [1] = 32253, -- Legionkiller
+    [1] = 32252, -- Nether Shadow Tunic
+    [1] = 32254, -- The Brutalizer
+    [1] = 32256, -- Waistwrap of Infinity
+    [1] = 32260, -- Choker of Endless Nightmares
+    [1] = 32255, -- Felstone Bulwark
+    [1] = 32250, -- Pauldrons of Abyssal Fury
+    [1] = 32262, -- Siphon of the Nathrezim
+    [1] = 32251, -- Wraps of Precise Flight
+    [1] = 32258, -- Naturalist's Preserving Cinch
+    [1] = 32259, -- Bands of the Coming Storm
+
+    -- Shade of Akama
+    [1] = 32266, -- Ring of Deceitful Intent
+    [1] = 32273, -- Amice of Brilliant Light
+    [1] = 32278, -- Grips of Silent Justice
+    [1] = 32263, -- Praetorian's Legguards
+    [1] = 32264, -- Shoulders of the Hidden Predator
+    [1] = 32270, -- Focused Mana Bindings
+    [1] = 32271, -- Kilt of Immortal Nature
+    [1] = 32268, -- Myrmidon's Treads
+    [1] = 32265, -- Shadow-walker's Cord
+    [1] = 32513, -- Wristbands of Divine Influence
+    [1] = 32361, -- Blind-Seers Icon
+    [1] = 32275, -- Spiritwalker Gauntlets
+    [1] = 32279, -- The Seeker's Wristguards
+    [1] = 32276, -- Flashfire Girdle
+
+    -- Teron Gorefiend
+    [1] = 32510, -- Softstep Boots of Tracking
+    [1] = 32328, -- Botanist's Gloves of Growth
+    [1] = 32329, -- Cowl of Benevolence
+    [1] = 32280, -- Gauntlets of Enforcement
+    [1] = 32512, -- Girdle of Lordaeron's Fallen
+    [1] = 32324, -- Insidious Bands
+    [1] = 32325, -- Rifle of the Stoic Guardian
+    [1] = 32327, -- Robe of the Shadow Council
+    [1] = 32323, -- Shadowmoon Destroyer's Drape
+    [1] = 32348, -- Soul Cleaver
+    [1] = 32330, -- Totem of Ancestral Guidance
+    [1] = 32326, -- Twisted Blades of Zarek
+
+
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
+    [1] = 99999, -- TODO
     [1] = 99999, -- TODO
 }

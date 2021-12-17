61d50c28a561fdd7e08cf2c0de2a9ff0bd6f0065
Matt Caruano <mjcengineer89@yahoo.com>
2021-12-16 23:12:40 -0800

    Finished adding BT loot. Still need to add the array index numbers, and still need to do Hyjal loot

--- TnTDKP/TierSixLoot.lua
+++ TnTDKP/TierSixLoot.lua
@@ -1,5 +1,24 @@
 -- This table simply contains the itemIDs of all loot from T6 content (BT/Hyjal)
 tierSixLoot = {
+
+    -- BT/Hyjal Shared Boss/Trash Patterns
+    [1] = 32739, -- Plans: Dawnsteel Shoulders
+    [1] = 32745, -- Pattern: Shoulderpads of Renewed Life
+    [1] = 32751, -- Pattern: Living Earth Shoulders
+    [1] = 32755, -- Pattern: Mantle of Nimble Thought
+    [1] = 32749, -- Pattern: Shoulders of Lightning Reflexes
+    [1] = 32753, -- Pattern: Swiftheal Mantle
+    [1] = 32737, -- Plans: Swiftsteel Shoulders
+    [1] = 32747, -- Pattern: Swiftstrike Shoulders
+
+    -- BT/Hyjal Patterns that are not LootConfig eligible (craft BoEs)
+    -- [1] = 32738, -- Plans: Dawnsteel Bracers
+    -- [1] = 32754, -- Pattern: Bracers of Nimble Thought
+    -- [1] = 32744, -- Pattern: Bracers of Renewed Life
+    -- [1] = 32750, -- Pattern: Living Earth Bindings
+    -- [1] = 32752, -- Pattern: Swiftheal Wraps
+
+
     -- BT Trash drops
     [1] = 32528, -- Blessed Band of Karabor
     [1] = 34011, -- Illidari Runeshield
@@ -9,27 +28,9 @@ tierSixLoot = {
     [1] = 99999, -- Swiftsteel Bludgeon
     [1] = 32606, -- Girdle of the Lightbearer
 
-    [1] = 32749, -- Pattern: Shoulders of Lightning Reflexes
-    [1] = 32753, -- Pattern: Swiftheal Mantle
-    [1] = 32737, -- Plans: Swiftsteel Shoulders
-    [1] = 32754, -- Pattern: Bracers of Nimble Thought
-    [1] = 32744, -- Pattern: Bracers of Renewed Life
-    [1] = 32750, -- Pattern: Living Earth Bindings
-    [1] = 32747, -- Pattern: Swiftstrike Shoulders
+    
 
-    -- BT Shared Boss Patterns
-    [1] = 32738, -- Plans: Dawnsteel Bracers
-    [1] = 32739, -- Plans: Dawnsteel Shoulders
-    [1] = 32745, -- Pattern: Shoulderpads of Renewed Life
-    [1] = 32751, -- Pattern: Living Earth Shoulders
-    [1] = 32755, -- Pattern: Mantle of Nimble Thought
-    [1] = 32752, -- Pattern: Swiftheal Wraps
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
+    
 
 
     -- High Warlord Naj'entus
@@ -93,40 +94,78 @@ tierSixLoot = {
     [1] = 32330, -- Totem of Ancestral Guidance
     [1] = 32326, -- Twisted Blades of Zarek
 
+    -- Gurtogg Bloodboil
+    [1] = 32334, -- Vest of Mounting Assault
+    [1] = 32333, -- Girdle of Stability
+    [1] = 32344, -- Staff of Immaculate Recovery
+    [1] = 32337, -- Shroud of Forgiveness
+    [1] = 32340, -- Garments of Temperance
+    [1] = 32341, -- Leggings of Divine Retribution
+    [1] = 32338, -- Blood-cursed Shoulderpads
+    [1] = 32269, -- Messenger of Fate
+    [1] = 32335, -- Unstoppable Aggressor's Ring
+    [1] = 32343, -- Wand of Prismatic Focus
+    [1] = 32339, -- Belt of Primal Majesty
+    [1] = 32501, -- Shadowmoon Insignia
+    [1] = 32342, -- Girdle of Mighty Resolve
 
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
+    -- Reliquary of Souls
+    [1] = 32346, -- Boneweave Girdle
+    [1] = 32354, -- Crown of Empowered Fate
+    [1] = 32345, -- Dreadboots of the Legion
+    [1] = 32351, -- Elunite Empowered Bracers
+    [1] = 32353, -- Gloves of Unfailing Faith
+    [1] = 32347, -- Grips of Damnation
+    [1] = 32363, -- Naaru-Blessed Life Rod
+    [1] = 32352, -- Naturewarden's Treads
+    [1] = 32362, -- Pendant of Titans
+    [1] = 32517, -- The Wavemender's Mantle
+    [1] = 32332, -- Torch of the Damned
+    [1] = 32350, -- Touch of Inspiration
+    [1] = 32349, -- Translucent Spellthread Necklace
+
+    -- Mother Shahraz
+    [1] = 31101, -- Pauldrons of the Forgotten Conqueror
+    [1] = 31103, -- Pauldrons of the Forgotten Protector
+    [1] = 31102, -- Pauldrons of the Forgotten Vanquisher
+    [1] = 32366, -- Shadowmaster's Boots
+    [1] = 32367, -- Leggings of Devastation
+    [1] = 32369, -- Blade of Savagery
+    [1] = 32370, -- Nadina's Pendant of Purity
+    [1] = 32365, -- Heartshatter Breastplate
+    [1] = 32368, -- Tome of the Lightbringer
+
+    -- The Illidari Council
+    [1] = 31098, -- Leggings of the Forgotten Conqueror
+    [1] = 31100, -- Leggings of the Forgotten Protector
+    [1] = 31099, -- Leggings of the Forgotten Vanquisher
+    [1] = 32519, -- Belt of Divine Guidance
+    [1] = 32331, -- Cloak of the Illidari Council
+    [1] = 32376, -- Forest Prowler's Helm
+    [1] = 32373, -- Helm of the Illidari Shatterer
+    [1] = 32505, -- Madness of the Betrayer
+    [1] = 32518, -- Veil of Turning Leaves
+
+    -- Illidan Stormrage
+    [1] = 31091, -- Chestguard of the Forgotten Protector
+    [1] = 31090, -- Chestguard of the Forgotten Vanquisher
+    [1] = 31089, -- Chestguard of the Forgotten Conqueror
+    [1] = 32483, -- The Skull of Gul'dan
+    [1] = 32336, -- Black Bow of the Betrayer
+    [1] = 32500, -- Crystal Spire of Karabor
+    [1] = 32496, -- Memento of Tyrande
+    [1] = 32375, -- Bulwark of Azzinoth
+    [1] = 32525, -- Cowl of the Illidari High Lord
+    [1] = 32235, -- Cursed Vision of Sargeras
+    [1] = 32521, -- Faceplate of the Impenetrable
+    [1] = 32471, -- Shard of Azzinoth
+    [1] = 32524, -- Shroud of the Highborne
+    [1] = 32497, -- Stormrage Signet Ring
+    [1] = 32374, -- Zhar'doom, Greatstaff of the Devourer
+    [1] = 32827, -- Warglaive of Azzinoth
+    [1] = 32828, -- Warglaive of Azzinoth
+
+    -- Hjyal Trash drops
     [1] = 99999, -- TODO
     [1] = 99999, -- TODO
     [1] = 99999, -- TODO

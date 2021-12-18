7ff52c51d660a4ca7db6854ce38f80702bcf2792
Matt Caruano <mjcengineer89@yahoo.com>
2021-12-17 16:01:50 -0800

    Finished adding T6 loot

--- TnTDKP/TierSixLoot.lua
+++ TnTDKP/TierSixLoot.lua
@@ -3,13 +3,13 @@ tierSixLoot = {
 
     -- BT/Hyjal Shared Boss/Trash Patterns
     [1] = 32739, -- Plans: Dawnsteel Shoulders
-    [1] = 32745, -- Pattern: Shoulderpads of Renewed Life
-    [1] = 32751, -- Pattern: Living Earth Shoulders
-    [1] = 32755, -- Pattern: Mantle of Nimble Thought
-    [1] = 32749, -- Pattern: Shoulders of Lightning Reflexes
-    [1] = 32753, -- Pattern: Swiftheal Mantle
-    [1] = 32737, -- Plans: Swiftsteel Shoulders
-    [1] = 32747, -- Pattern: Swiftstrike Shoulders
+    [2] = 32745, -- Pattern: Shoulderpads of Renewed Life
+    [3]] = 32751, -- Pattern: Living Earth Shoulders
+    [4] = 32755, -- Pattern: Mantle of Nimble Thought
+    [5] = 32749, -- Pattern: Shoulders of Lightning Reflexes
+    [6] = 32753, -- Pattern: Swiftheal Mantle
+    [7] = 32737, -- Plans: Swiftsteel Shoulders
+    [8] = 32747, -- Pattern: Swiftstrike Shoulders
 
     -- BT/Hyjal Patterns that are not LootConfig eligible (craft BoEs)
     -- [1] = 32738, -- Plans: Dawnsteel Bracers
@@ -18,165 +18,225 @@ tierSixLoot = {
     -- [1] = 32750, -- Pattern: Living Earth Bindings
     -- [1] = 32752, -- Pattern: Swiftheal Wraps
 
-
     -- BT Trash drops
-    [1] = 32528, -- Blessed Band of Karabor
-    [1] = 34011, -- Illidari Runeshield
-    [1] = 32526, -- Band of Devastation
-    [1] = 32527, -- Ring of Ancient Knowledge
-    [1] = 99999, -- Shroud of the Final Stand
-    [1] = 99999, -- Swiftsteel Bludgeon
-    [1] = 32606, -- Girdle of the Lightbearer
-
-    
-
-    
-
+    [9] = 32528, -- Blessed Band of Karabor
+    [10] = 34011, -- Illidari Runeshield
+    [11] = 32526, -- Band of Devastation
+    [12] = 32527, -- Ring of Ancient Knowledge
+    [13] = 99999, -- Shroud of the Final Stand
+    [14] = 99999, -- Swiftsteel Bludgeon
+    [15] = 32606, -- Girdle of the Lightbearer
 
     -- High Warlord Naj'entus
-    [1] = 32247, -- Ring of Captured Storms
-    [1] = 32377, -- Mantle of Darkness
-    [1] = 32238, -- Ring of Calming Waves
-    [1] = 32234, -- Fists of Mukoa
-    [1] = 32248, -- Halberd of Desolation
-    [1] = 32239, -- Slippers of the Seacaller
-    [1] = 32237, -- The Maelstrom's Fury
-    [1] = 32232, -- Eternium Shell Bracers
-    [1] = 32240, -- Guise of the Tidal Lurker
-    [1] = 32236, -- Rising Tide
-    [1] = 32243, -- Pearl Inlaid Boots
-    [1] = 32241, -- Helm of Soothing Currents
-    [1] = 32242, -- Boots of Oceanic Fury
-    [1] = 32245, -- Tide-stomper's Greaves
+    [16] = 32247, -- Ring of Captured Storms
+    [17] = 32377, -- Mantle of Darkness
+    [18] = 32238, -- Ring of Calming Waves
+    [19] = 32234, -- Fists of Mukoa
+    [20] = 32248, -- Halberd of Desolation
+    [21] = 32239, -- Slippers of the Seacaller
+    [22] = 32237, -- The Maelstrom's Fury
+    [23] = 32232, -- Eternium Shell Bracers
+    [24] = 32240, -- Guise of the Tidal Lurker
+    [25] = 32236, -- Rising Tide
+    [26] = 32243, -- Pearl Inlaid Boots
+    [27] = 32241, -- Helm of Soothing Currents
+    [28] = 32242, -- Boots of Oceanic Fury
+    [29] = 32245, -- Tide-stomper's Greaves
 
     -- Supremus
-    [1] = 32261, -- Band of the Abyssal Lord
-    [1] = 32257, -- Idol of the White Stag
-    [1] = 32253, -- Legionkiller
-    [1] = 32252, -- Nether Shadow Tunic
-    [1] = 32254, -- The Brutalizer
-    [1] = 32256, -- Waistwrap of Infinity
-    [1] = 32260, -- Choker of Endless Nightmares
-    [1] = 32255, -- Felstone Bulwark
-    [1] = 32250, -- Pauldrons of Abyssal Fury
-    [1] = 32262, -- Siphon of the Nathrezim
-    [1] = 32251, -- Wraps of Precise Flight
-    [1] = 32258, -- Naturalist's Preserving Cinch
-    [1] = 32259, -- Bands of the Coming Storm
+    [30] = 32261, -- Band of the Abyssal Lord
+    [31] = 32257, -- Idol of the White Stag
+    [32] = 32253, -- Legionkiller
+    [33] = 32252, -- Nether Shadow Tunic
+    [34] = 32254, -- The Brutalizer
+    [35] = 32256, -- Waistwrap of Infinity
+    [36] = 32260, -- Choker of Endless Nightmares
+    [37] = 32255, -- Felstone Bulwark
+    [38] = 32250, -- Pauldrons of Abyssal Fury
+    [39] = 32262, -- Siphon of the Nathrezim
+    [40] = 32251, -- Wraps of Precise Flight
+    [41] = 32258, -- Naturalist's Preserving Cinch
+    [42] = 32259, -- Bands of the Coming Storm
 
     -- Shade of Akama
-    [1] = 32266, -- Ring of Deceitful Intent
-    [1] = 32273, -- Amice of Brilliant Light
-    [1] = 32278, -- Grips of Silent Justice
-    [1] = 32263, -- Praetorian's Legguards
-    [1] = 32264, -- Shoulders of the Hidden Predator
-    [1] = 32270, -- Focused Mana Bindings
-    [1] = 32271, -- Kilt of Immortal Nature
-    [1] = 32268, -- Myrmidon's Treads
-    [1] = 32265, -- Shadow-walker's Cord
-    [1] = 32513, -- Wristbands of Divine Influence
-    [1] = 32361, -- Blind-Seers Icon
-    [1] = 32275, -- Spiritwalker Gauntlets
-    [1] = 32279, -- The Seeker's Wristguards
-    [1] = 32276, -- Flashfire Girdle
+    [43] = 32266, -- Ring of Deceitful Intent
+    [44] = 32273, -- Amice of Brilliant Light
+    [45] = 32278, -- Grips of Silent Justice
+    [46] = 32263, -- Praetorian's Legguards
+    [47] = 32264, -- Shoulders of the Hidden Predator
+    [48] = 32270, -- Focused Mana Bindings
+    [49] = 32271, -- Kilt of Immortal Nature
+    [50] = 32268, -- Myrmidon's Treads
+    [51] = 32265, -- Shadow-walker's Cord
+    [52] = 32513, -- Wristbands of Divine Influence
+    [53] = 32361, -- Blind-Seers Icon
+    [54] = 32275, -- Spiritwalker Gauntlets
+    [55] = 32279, -- The Seeker's Wristguards
+    [56] = 32276, -- Flashfire Girdle
 
     -- Teron Gorefiend
-    [1] = 32510, -- Softstep Boots of Tracking
-    [1] = 32328, -- Botanist's Gloves of Growth
-    [1] = 32329, -- Cowl of Benevolence
-    [1] = 32280, -- Gauntlets of Enforcement
-    [1] = 32512, -- Girdle of Lordaeron's Fallen
-    [1] = 32324, -- Insidious Bands
-    [1] = 32325, -- Rifle of the Stoic Guardian
-    [1] = 32327, -- Robe of the Shadow Council
-    [1] = 32323, -- Shadowmoon Destroyer's Drape
-    [1] = 32348, -- Soul Cleaver
-    [1] = 32330, -- Totem of Ancestral Guidance
-    [1] = 32326, -- Twisted Blades of Zarek
+    [57] = 32510, -- Softstep Boots of Tracking
+    [58] = 32328, -- Botanist's Gloves of Growth
+    [59] = 32329, -- Cowl of Benevolence
+    [60] = 32280, -- Gauntlets of Enforcement
+    [61] = 32512, -- Girdle of Lordaeron's Fallen
+    [62] = 32324, -- Insidious Bands
+    [63] = 32325, -- Rifle of the Stoic Guardian
+    [64] = 32327, -- Robe of the Shadow Council
+    [65] = 32323, -- Shadowmoon Destroyer's Drape
+    [66] = 32348, -- Soul Cleaver
+    [67] = 32330, -- Totem of Ancestral Guidance
+    [68] = 32326, -- Twisted Blades of Zarek
 
     -- Gurtogg Bloodboil
-    [1] = 32334, -- Vest of Mounting Assault
-    [1] = 32333, -- Girdle of Stability
-    [1] = 32344, -- Staff of Immaculate Recovery
-    [1] = 32337, -- Shroud of Forgiveness
-    [1] = 32340, -- Garments of Temperance
-    [1] = 32341, -- Leggings of Divine Retribution
-    [1] = 32338, -- Blood-cursed Shoulderpads
-    [1] = 32269, -- Messenger of Fate
-    [1] = 32335, -- Unstoppable Aggressor's Ring
-    [1] = 32343, -- Wand of Prismatic Focus
-    [1] = 32339, -- Belt of Primal Majesty
-    [1] = 32501, -- Shadowmoon Insignia
-    [1] = 32342, -- Girdle of Mighty Resolve
+    [69] = 32334, -- Vest of Mounting Assault
+    [70] = 32333, -- Girdle of Stability
+    [71] = 32344, -- Staff of Immaculate Recovery
+    [72] = 32337, -- Shroud of Forgiveness
+    [73] = 32340, -- Garments of Temperance
+    [74] = 32341, -- Leggings of Divine Retribution
+    [75] = 32338, -- Blood-cursed Shoulderpads
+    [76] = 32269, -- Messenger of Fate
+    [77] = 32335, -- Unstoppable Aggressor's Ring
+    [78] = 32343, -- Wand of Prismatic Focus
+    [79] = 32339, -- Belt of Primal Majesty
+    [80] = 32501, -- Shadowmoon Insignia
+    [81] = 32342, -- Girdle of Mighty Resolve
 
     -- Reliquary of Souls
-    [1] = 32346, -- Boneweave Girdle
-    [1] = 32354, -- Crown of Empowered Fate
-    [1] = 32345, -- Dreadboots of the Legion
-    [1] = 32351, -- Elunite Empowered Bracers
-    [1] = 32353, -- Gloves of Unfailing Faith
-    [1] = 32347, -- Grips of Damnation
-    [1] = 32363, -- Naaru-Blessed Life Rod
-    [1] = 32352, -- Naturewarden's Treads
-    [1] = 32362, -- Pendant of Titans
-    [1] = 32517, -- The Wavemender's Mantle
-    [1] = 32332, -- Torch of the Damned
-    [1] = 32350, -- Touch of Inspiration
-    [1] = 32349, -- Translucent Spellthread Necklace
+    [82] = 32346, -- Boneweave Girdle
+    [83] = 32354, -- Crown of Empowered Fate
+    [84] = 32345, -- Dreadboots of the Legion
+    [85] = 32351, -- Elunite Empowered Bracers
+    [86] = 32353, -- Gloves of Unfailing Faith
+    [87] = 32347, -- Grips of Damnation
+    [88] = 32363, -- Naaru-Blessed Life Rod
+    [89] = 32352, -- Naturewarden's Treads
+    [90] = 32362, -- Pendant of Titans
+    [91] = 32517, -- The Wavemender's Mantle
+    [92] = 32332, -- Torch of the Damned
+    [93] = 32350, -- Touch of Inspiration
+    [94] = 32349, -- Translucent Spellthread Necklace
 
     -- Mother Shahraz
-    [1] = 31101, -- Pauldrons of the Forgotten Conqueror
-    [1] = 31103, -- Pauldrons of the Forgotten Protector
-    [1] = 31102, -- Pauldrons of the Forgotten Vanquisher
-    [1] = 32366, -- Shadowmaster's Boots
-    [1] = 32367, -- Leggings of Devastation
-    [1] = 32369, -- Blade of Savagery
-    [1] = 32370, -- Nadina's Pendant of Purity
-    [1] = 32365, -- Heartshatter Breastplate
-    [1] = 32368, -- Tome of the Lightbringer
+    [95] = 31101, -- Pauldrons of the Forgotten Conqueror
+    [96] = 31103, -- Pauldrons of the Forgotten Protector
+    [97] = 31102, -- Pauldrons of the Forgotten Vanquisher
+    [98] = 32366, -- Shadowmaster's Boots
+    [99] = 32367, -- Leggings of Devastation
+    [100] = 32369, -- Blade of Savagery
+    [101] = 32370, -- Nadina's Pendant of Purity
+    [102] = 32365, -- Heartshatter Breastplate
+    [103] = 32368, -- Tome of the Lightbringer
 
     -- The Illidari Council
-    [1] = 31098, -- Leggings of the Forgotten Conqueror
-    [1] = 31100, -- Leggings of the Forgotten Protector
-    [1] = 31099, -- Leggings of the Forgotten Vanquisher
-    [1] = 32519, -- Belt of Divine Guidance
-    [1] = 32331, -- Cloak of the Illidari Council
-    [1] = 32376, -- Forest Prowler's Helm
-    [1] = 32373, -- Helm of the Illidari Shatterer
-    [1] = 32505, -- Madness of the Betrayer
-    [1] = 32518, -- Veil of Turning Leaves
+    [104] = 31098, -- Leggings of the Forgotten Conqueror
+    [105] = 31100, -- Leggings of the Forgotten Protector
+    [106] = 31099, -- Leggings of the Forgotten Vanquisher
+    [107] = 32519, -- Belt of Divine Guidance
+    [108] = 32331, -- Cloak of the Illidari Council
+    [109] = 32376, -- Forest Prowler's Helm
+    [110] = 32373, -- Helm of the Illidari Shatterer
+    [111] = 32505, -- Madness of the Betrayer
+    [112] = 32518, -- Veil of Turning Leaves
 
     -- Illidan Stormrage
-    [1] = 31091, -- Chestguard of the Forgotten Protector
-    [1] = 31090, -- Chestguard of the Forgotten Vanquisher
-    [1] = 31089, -- Chestguard of the Forgotten Conqueror
-    [1] = 32483, -- The Skull of Gul'dan
-    [1] = 32336, -- Black Bow of the Betrayer
-    [1] = 32500, -- Crystal Spire of Karabor
-    [1] = 32496, -- Memento of Tyrande
-    [1] = 32375, -- Bulwark of Azzinoth
-    [1] = 32525, -- Cowl of the Illidari High Lord
-    [1] = 32235, -- Cursed Vision of Sargeras
-    [1] = 32521, -- Faceplate of the Impenetrable
-    [1] = 32471, -- Shard of Azzinoth
-    [1] = 32524, -- Shroud of the Highborne
-    [1] = 32497, -- Stormrage Signet Ring
-    [1] = 32374, -- Zhar'doom, Greatstaff of the Devourer
-    [1] = 32827, -- Warglaive of Azzinoth
-    [1] = 32828, -- Warglaive of Azzinoth
+    [113] = 31091, -- Chestguard of the Forgotten Protector
+    [114] = 31090, -- Chestguard of the Forgotten Vanquisher
+    [115] = 31089, -- Chestguard of the Forgotten Conqueror
+    [116] = 32483, -- The Skull of Gul'dan
+    [117] = 32336, -- Black Bow of the Betrayer
+    [118] = 32500, -- Crystal Spire of Karabor
+    [119] = 32496, -- Memento of Tyrande
+    [120] = 32375, -- Bulwark of Azzinoth
+    [121] = 32525, -- Cowl of the Illidari High Lord
+    [122] = 32235, -- Cursed Vision of Sargeras
+    [123] = 32521, -- Faceplate of the Impenetrable
+    [124] = 32471, -- Shard of Azzinoth
+    [125] = 32524, -- Shroud of the Highborne
+    [126] = 32497, -- Stormrage Signet Ring
+    [127] = 32374, -- Zhar'doom, Greatstaff of the Devourer
+    [128] = 32827, -- Warglaive of Azzinoth
+    [129] = 32828, -- Warglaive of Azzinoth
 
     -- Hjyal Trash drops
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
-    [1] = 99999, -- TODO
+    [130] = 32609, -- Boots of the Divine Light
+    [131] = 32592, -- Chestguard of Relentless Storms
+    [132] = 32591, -- Choker of Serrated Blades
+    [133] = 32946, -- Claw of Molten Fury
+    [134] = 32945, -- Fist of Molten Fury
+    [135] = 34009, -- Hammer of Judgement
+    [136] = 32589, -- Hellfire-Encased Pendant
+    [137] = 32590, -- Nethervoid Cloak
+    [138] = 34010, -- Pepe's Shroud of Pacification
+
+    -- Rage Winterchill
+    [139] = 30862, -- Blessed Adamantite Bracers
+    [140] = 30870, -- Cuffs of Devastation
+    [141] = 30871, -- Bracers of Martyrdom
+    [142] = 30872, -- Chronicle of Dark Secrets
+    [143] = 30868, -- Rejuvenating Bracers
+    [144] = 30866, -- Blood-stained Pauldrons
+    [145] = 30864, -- Bracers of the Pathfinder
+    [146] = 30865, -- Tracker's Blade
+    [147] = 30863, -- Deadly Cuffs
+    [148] = 30861, -- Furious Shackles
+    [149] = 30869, -- Howling Wind Bracers
+    [150] = 30873, -- Stillwater Boots
+
+    -- Anetheron
+    [151] = 30887, -- Golden Links of Restoration
+    [152] = 30879, -- Don Alejandro's Money Belt
+    [153] = 30888, -- Anetheron's Noose
+    [154] = 30878, -- Glimmering Steel Mantle
+    [155] = 30883, -- Pillar of Ferocity
+    [156] = 30874, -- The Unbreakable Will
+    [157] = 30881, -- Blade of Infamy
+    [158] = 30886, -- Enchanted Leather Sandals
+    [159] = 30884, -- Hatefury Mantle
+    [160] = 30880, -- Quickstrider Moccasins
+    [161] = 30885, -- Archbishop's Slippers
+    [162] = 30882, -- Bastion of Light
+
+    -- Kaz'rogal
+    [163] = 30918, -- Hammer of Atonement
+    [164] = 30916, -- Leggings of Channeled Elements
+    [165] = 30917, -- Razorfury Mantle
+    [166] = 30891, -- Black Featherlight Boots
+    [167] = 30919, -- Valestalker Girdle
+    [168] = 30892, -- Beast-tamer's Shoulders
+    [169] = 30915, -- Belt of Seething Fury
+    [170] = 30895, -- Angelista's Sash
+    [171] = 30894, -- Blue Suede Shoes
+    [172] = 30889, -- Kaz'rogal's Hardened Heart
+    [173] = 30893, -- Sun-touched Chain Leggings
+    [174] = 30914, -- Belt of the Crescent Moon
+
+    -- Azgalor
+    [175] = 31093, -- Gloves of the Forgotten Vanquisher
+    [176] = 31094, -- Gloves of the Forgotten Protector
+    [177] = 31092, -- Gloves of the Forgotten Conqueror
+    [178] = 30897, -- Girdle of Hope
+    [179] = 30900, -- Bow-stitched leggings
+    [180] = 30899, -- Don Rodrigo's Poncho
+    [181] = 30896, -- Glory of the Defender
+    [182] = 30901, -- Boundless Agony
+    [183] = 30898, -- Shady Dealer's Pantaloons
+
+    -- Archimonde
+    [184] = 31096, -- Helm of the Forgotten Vanquisher
+    [185] = 31097, -- Helm of the Forgotten Conqueror
+    [186] = 31095, -- Helm of the Forgotten Protector
+    [187] = 30906, -- Bristleblitz Striker
+    [188] = 30907, -- Mail of Fevered Pursuit
+    [189] = 30913, -- Robes of Rhonin
+    [190] = 30904, -- Savior's Grasp
+    [191] = 30911, -- Scepter of Purification
+    [192] = 30910, -- Tempest of Chaos
+    [193] = 30908, -- Apostle of Argus
+    [194] = 30912, -- Leggings of Eternity
+    [195] = 30905, -- Midnight Chestguard
+    [196] = 30903, -- Legguards of Endless Rage
+    [197] = 30902, -- Cataclysm's Edge
+    [198] = 30909, -- Antonidas's Aegis of Rapt Concentration
 }

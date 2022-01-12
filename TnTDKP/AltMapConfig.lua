2fa20b2971ce6e2409d39932dd779657b6fa7bc9
Mcaruano <mjcengineer89@yahoo.com>
2022-01-12 09:31:17 -0800

    Overhauled the AltMap for Benediction
    Updated all of the alts (and mains) in the AltMap to reflect the new names on Benediction. Also completely dropped all other alts that weren't declared by players

--- TnTDKP/AltMapConfig.lua
+++ TnTDKP/AltMapConfig.lua
@@ -2,110 +2,8 @@
 -- we might need for a given encounter still get their DKP awards attributed
 -- to their "main" character
 altMapConfig = {
-	-- Malch's Rogue
-	["Stacylet"] = "Malchazor",
-
-	-- Malch's Druid
-	["Messao"] = "Malchazor",
-
-	-- Malch's Shaman
-	["Shmalchazor"] = "Malchazor",
-
-	-- Searus' Rogue
-	["Searus"] = "Searious",
-
-	-- Searus' Warrior
-	["Seariously"] = "Searious",
-
-	-- Searus' Shaman
-	["Onetotemman"] = "Searious",
-
-	-- Searus' Paladin
-	["Searusbuffs"] = "Searious",
-
-	-- Kang's Paladin
-	["Blessbeard"] = "Kang",
-
-	-- Kang's Warlock
-	["Memenology"] = "Kang",
-
-	-- Kang's Hunter
-	["Kangbank"] = "Kang",
-
-	-- Kang's Rogue
-	["Furtive"] = "Kang",
-
-	-- Kang's Mage
-	["Bankdude"] = "Kang",
-
-	-- Kang's Druid
-	["Weedster"] = "Kang",
-
-	-- Ashori's Priest
-	["Amilea"] = "Ashori",
-
-	-- Ashori's Rogue
-	["Hevalin"] = "Ashori",
-
-	-- Ashori's Mage
-	["Coldrage"] = "Ashori",
-
-	-- Rotheart's Warrior
-	["Meatcleave"] = "Rotheart",
-
-	-- Rotheart's Mage
-	["Allara"] = "Rotheart",
-
-	-- Rotheart's Rogue
-	["Gkp"] = "Rotheart",
-
-	-- Mlg's Rogue
-	["Backhugs"] = "Mlg",
-
-	-- Mlg's Priest
-	["Babydwarf"] = "Mlg",
-
-	-- Mlg's Warrior
-	["Ltc"] = "Mlg",
-
-	-- Mlg's Hunter
-	["Vipershot"] = "Mlg",
-
-	-- Dalran's other Warrior
-	["Daleran"] = "Dalran",
-
-	-- Dalran's Hunter
-	["Dalryn"] = "Dalran",
-
-	-- Demeker's Rogue
-	["Demek"] = "Demeker",
-
-	-- Demeker's Druid
-	["Ristin"] = "Demeker",
-
-	-- Demeker's Hunter
-	["Driffa"] = "Demeker",
-
-	-- Realfrosted's Mage
-	["Frósted"] = "Realfrosted",
-
-	-- Realfrosted's Paladin
-	["Flakés"] = "Realfrosted",
-
-	-- Realfrosted's Warrior
-	["Frosteds"] = "Realfrosted",
-
-	-- Realfrosted's Druid
-	["Squawker"] = "Realfrosted",
-
-	-- Sofakinglulz's Warlock
-	["Painnsufferz"] = "Sofakinglulz",
-
-	-- Sofakinglulz's Mage
-	["Blisterz"] = "Sofakinglulz",
-
-	-- Aro's Rogue
-	["Uhuh"] = "Aro",
+	-- Abjection's other Hunter
+	["Malignance"] = "Abjection",
 
 	-- Akaran's Warlock
 	["Shcout"] = "Akaran",
@@ -122,8 +20,65 @@ altMapConfig = {
 	-- Akaran's Paladin
 	["Shnaxxorcism"] = "Akaran",
 
-	-- Shnacks' Paladin
-	["Anarra"] = "Shnacks",
+	-- Ashori's Priest
+	["Naliveh"] = "Ashori",
+
+	-- Baopi's Shaman
+	["Mikoyagi"] = "Baopi",
+
+	-- Barnabae's Warrior
+	["Hulkhigginz"] = "Barnabae",
+
+	-- Barnabae's Priest
+	["Higginzz"] = "Barnabae",
+
+	-- Crazydaisy's Mage
+	["Manarolls"] = "Crazydaisy",
+
+	-- Crazydaisy's Warrior
+	["Snickercakes"] = "Crazydaisy",
+
+	-- Demeker's Druid
+	["Ristin"] = "Demeker",
+
+    -- Digie's Paladin
+	["Digitoh"] = "Digie",
+
+	-- Digie's Hunter
+	["Digitizzle"] = "Digie",
+
+	-- Ellievyra's Warlock
+	["Bryandee"] = "Ellievyra",
+
+	-- Hyolin's Paladin
+	["Envisage"] = "Hyolin",
+
+	-- Justin's Warrior
+	["Justinbiceps"] = "Justin",
+
+	-- Justin's Druid
+	["Justinsr"] = "Justin",
+
+	-- Kinestra's Warrior
+	["Kineztra"] = "Kinestra",
+
+	-- Nashborne's Mage
+	["Eneldy"] = "Nashborne",
+
+	-- Rayashaman's Paladin
+	["Rayapaladin"] = "Rayashaman",
+
+	-- Rayashaman's Mage
+	["Rayamage"] = "Rayashaman",
+
+	-- Searious' Rogue
+	["Searus"] = "Searious",
+
+	-- Searious' Warrior
+	["Seariously"] = "Searious",
+
+	-- Searious' Shaman
+	["Shearious"] = "Searious",
 
 	-- Shnacks' Mage
 	["Shnackattack"] = "Shnacks",
@@ -137,276 +92,9 @@ altMapConfig = {
 	-- Shnacks' Warlock
 	["Shpookyshnax"] = "Shnacks",
 
-	-- Sylador's Mage
-	["Autist"] = "Sylador",
-
 	-- Sylador's Paladin
 	["Silador"] = "Sylador",
 
-	-- Galm's Hunter
-	["Gilanerth"] = "Galm",
-
-	-- Galm's Other Mage
-	["Lucien"] = "Galm",
-
-	-- Galm's Druid
-	["Caledhel"] = "Galm",
-
-	-- Galm's Paladin
-	["Vingilot"] = "Galm",
-
-	-- Rumegirl's Mage
-	["Rumeboi"] = "Rumegirl",
-
-	-- Rumegirl's Hunter
-	["Rumeboy"] = "Rumegirl",
-
-	-- Rumegirl's Priest
-	["Rumelol"] = "Rumegirl",
-
-	-- Zerxx's Mage
-	["Metrixx"] = "Zerxx",
-
-	-- Justin's Warrior
-	["Oní"] = "Justin",
-
-	-- Justin's Druid
-	["Justinjr"] = "Justin",
-
-	-- Lynn's Paladin
-	["Christiene"] = "Caelandine",
-
-	-- Lynn's Rogue
-	["Kierandros"] = "Caelandine",
-
-	-- Berzerk's Paladin
-	["Padaladin"] = "Berzerk",
-
-	-- Digie's Paladin
-	["Digiret"] = "Digie",
-
-	-- Digie's Hunter
-	["Digitoh"] = "Digie",
-
-	-- Radicola's Paladin
-	["Shordie"] = "Radicola",
-
-	-- Nashy's Mage
-	["Eneldy"] = "Nashy",
-
-	-- Nashy's Priest
-	["Eneldi"] = "Nashy",
-
-	-- Nashy's Druid
-	["Neldy"] = "Nashy",
-
-	-- Sekkondary's Warlock
-	["Twosek"] = "Sekkondary",
-
-	-- Sekkondary's Paladin
-	["Sekjr"] = "Sekkondary",
-
-	-- Haytme's Priest
-	["Elliee"] = "Haytme",
-
-	-- Roodolph's Rogue
-	["Stealthgiff"] = "Roodolph",
-
-	-- Roodolph's Mage
-	["Archgiff"] = "Roodolph",
-
-	-- Roodolph's NE Hunter
-	["Rudolf"] = "Roodolph",
-
-	-- Roodolph's Paladin
-	["Holygiff"] = "Roodolph",
-
-	-- Phi's Paladin
-	["Phidose"] = "Phi",
-
-	-- Phi's Druid
-	["Shiftphi"] = "Phi",
-
-	-- Pikachau's Warrior
-	["Raichau"] = "Pikachau",
-
-	-- Pikachau's Mage
-	["Pikamage"] = "Pikachau",
-
-	-- Drunkinrage's Warlock
-	["Drunkinfears"] = "Drunkinrage",
-
-	-- Drunkinrage's Rogue
-	["Drunkinninja"] = "Drunkinrage",
-
-	-- Beansv's Mage
-	["Bevns"] = "Beansv",
-
-	-- Beansv's Hunter
-	["Beansii"] = "Beansv",
-
-	-- Whiskeysour's Paladin
-	["Fjori"] = "Whiskeysour",
-
-	-- Whiskeysour's Priest
-	["Mimosa"] = "Whiskeysour",
-
-	-- Whiskeysour's Warrior
-	["Irishcoffee"] = "Whiskeysour",
-
-	-- Whiskeysour's Mage
-	["Vodkatonic"] = "Whiskeysour",
-
-	-- Glim's other Mage
-	["Notglim"] = "Glimfolkor",
-
-	-- Barnabris' Paladin
-	["Higginz"] = "Barnabris",
-
-	-- Barnabris' Warrior
-	["Hulkhigginz"] = "Barnabris",
-
-	-- Barnabris' Priest
-	["Fearwardz"] = "Barnabris",
-
-	-- Barnabris' Druid
-	["Cyclonespam"] = "Barnabris",
-
-	-- Salchant's Hunter
-	["Putri"] = "Salchant",
-
-	-- Scrodo's Rogue
-	["Poekin"] = "Scrodo",
-
-	-- Scrodo's Hunter
-	["Boekin"] = "Scrodo",
-
-	-- Scrodo's other Mage
-	["Wankdalf"] = "Scrodo",
-
-	-- Hyolin's Druid
-	["Envishift"] = "Hyolin",
-
-	-- Hyolin's Hunter
-	["Envigunner"] = "Hyolin",
-
-	-- Hyolin's Paladin
-	["Envisage"] = "Hyolin",
-
-	-- Hyolin's Shaman
-	["Heatriser"] = "Hyolin",
-
-	-- Chuunin's other Rogue
-	["Choonin"] = "Chuunin",
-
-	-- Chuunin's Mage
-	["Frostone"] = "Chuunin",
-
-	-- Chuunin's New Mage
-	["Arcaneburden"] = "Chuunin",
-
-	-- Sizzle's Paladin
-	["Arklight"] = "Sizzleñips",
-
-	-- Sizzle's Warrior
-	["Àrkadious"] = "Sizzleñips",
-
-	-- Peacard's Paladin
-	["Kidboomer"] = "Peacard",
-
-	-- Ellievyra's Druid
-	["Mmtsunami"] = "Ellievyra",
-
-	-- Ellievyra's Warlock
-	["Hamiltonadam"] = "Ellievyra",
-
-	-- Murduc's Paladin
-	["Lancetankman"] = "Murduc",
-
-	-- Drizzyydrake's Rogue
-	["Youngdrizzy"] = "Drizzyydrake",
-
-	-- Drizzyydrake's Hunter
-	["Riskyrizzy"] = "Drizzyydrake",
-
-	-- Drizzyydrake's Mage
-	["Drizzyy"] = "Drizzyydrake",
-
-	-- Drizzyydrake's Warrior
-	["Deerizzy"] = "Drizzyydrake",
-
-	-- Wildshrimp's Other Warrior
-	["Shrimptease"] = "Wildshrimp",
-
-	-- Wildshrimp's Rogue
-	["Babyshrimp"] = "Wildshrimp",
-
-	-- Wildshrimp's Warrior
-	["Simptease"] = "Wildshrimp",
-
-	-- Wildshrimp's Paladin
-	["Andjelleh"] = "Wildshrimp",
-
-	-- Wildshrimp's Priest
-	["Thizguy"] = "Wildshrimp",
-
-	-- Wildshrimp's Warlock
-	["Dottyshrimp"] = "Wildshrimp",
-
-	-- Wildshrimp's Mage
-	["Shrimptea"] = "Wildshrimp",
-
-	-- Wildshrimp's Hunter
-	["Shrimpea"] = "Wildshrimp",
-
-	-- Goodjorb's Priest
-	["Holyjorb"] = "Goodjorb",
-
-	-- Vermora's Paladin
-	["Solljus"] = "Vermora",
-
-	-- Vermora's Warlock
-	["Lunarie"] = "Vermora",
-
-	-- Crazydaisy's Mage
-	["Manarolls"] = "Crazydaisy",
-
-	-- Crazydaisy's Mage
-	["Snickercakes"] = "Crazydaisy",
-
-	-- Kinastra's Priest
-	["Kinasa"] = "Kinastra",
-
-	-- Kinastra's Warrior
-	["Kinyonga"] = "Kinastra",
-
-	-- Kinastra's Rogue
-	["Kineztra"] = "Kinastra",
-
-	-- Rayas' Paladin
-	["Ráya"] = "Rayas",
-
-	-- Barnabris' Mage
-	["Higginzz"] = "Barnabris",
-
-	-- Zeion's Shaman
-	["Seionish"] = "Zeion",
-
-	-- Zeion's Warlock
-	["Seion"] = "Zeion",
-
-	-- Solzar's Warrior
-	["Kharagan"] = "Solzar",
-
-	-- Baopi's Shaman
-	["Mikoyagi"] = "Baopi",
-
-	-- Velyri's Shaman
-	["Velyri"] = "Velyris",
-
-	-- Remi's Warrior
-	["Reminosc"] = "Spookyremi",
-
-	-- Remi's Druid
-	["Reminosk"] = "Spookyremi",
+	-- Wildshrimp's Shaman
+	["Shramps"] = "Wildshrimp",
 }

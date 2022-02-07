7754fe019321663557528846c9249b760507194d
Mcaruano <mjcengineer89@yahoo.com>
2022-02-07 09:02:52 -0800

    Rebalanced T5 Boss DKP Awards
    Configured it so that Vashj & KT award 100% of the weekly T5 DKP allotment between the two of them

--- TnTDKP/TierFiveBossConfig.lua
+++ TnTDKP/TierFiveBossConfig.lua
@@ -1,42 +1,10 @@
 -- The configuration which hosts boss-specific configuration variables.
 tierFiveBossConfig = {	
-	["Hydross the Unstable"] = {
-		["KillDKPAward"] = 100,
-	},
-
-	["The Lurker Below"] = {
-		["KillDKPAward"] = 100,
-	},
-
-	["Leotheras the Blind"] = {
-		["KillDKPAward"] = 150,
-	},
-
-	["Fathom-Lord Karathress"] = {
-		["KillDKPAward"] = 100,
-	},
-
-	["Morogrim Tidewalker"] = {
-		["KillDKPAward"] = 100,
-	},
-
 	["Lady Vashj"] = {
-		["KillDKPAward"] = 180,
-	},
-
-	["Al'ar"] = {
-		["KillDKPAward"] = 100,
-	},
-
-	["Void Reaver"] = {
-		["KillDKPAward"] = 100,
-	},
-
-	["High Astromancer Solarian"] = {
-		["KillDKPAward"] = 100,
+		["KillDKPAward"] = 590,
 	},
 
 	["Kael'thas Sunstrider"] = {
-		["KillDKPAward"] = 150,
+		["KillDKPAward"] = 590,
 	},
 }

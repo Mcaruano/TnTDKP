b12e2ef19fda51874161e314c8ae8cb4df80a31a
Mcaruano <mjcengineer89@yahoo.com>
2022-04-18 18:07:47 -0700

    Added loot for T6.5 content

--- TnTDKP/TierSixPointFiveLoot.lua
+++ TnTDKP/TierSixPointFiveLoot.lua
@@ -1,5 +1,168 @@
--- This table simply contains the itemIDs of all loot from T4 content (Magtheridon/Gruul's Lair)
+-- This table simply contains the itemIDs of all loot from "T6.5" content (Sunwell)
 tierSixPointFiveLoot = {
+    -- Patterns that produce BoP items
+    [1] = 35216, -- Pattern: Leather Chestguard of the Sun
+    [2] = 35217, -- Pattern: Embrace of the Phoenix
+    [3] = 35218, -- Pattern: Carapace of Sun and Shadow
+    [4] = 35219, -- Pattern: Sun-Drenched Scale Chestguard
+    [5] = 35206, -- Pattern: Sunfire Robe
+    [6] = 35207, -- Pattern: Robe of Eternal Light
+    [7] = 35201, -- Design: Pendant of Sunfire
+    [8] = 35202, -- Design: Amulet of Flowing Life
+    [9] = 35203, -- Design: Hardened Khorium Choker
+    [10] = 35186, -- Schematic: Annihilator Holo-Gogs
+    [11] = 35187, -- Schematic: Justicebringer 3000 Specs
+    [12] = 35189, -- Schematic: Powerheal 9000 Lens
+    [13] = 35190, -- Schematic: Hyper-Magnified Moon Specs
+    [14] = 35191, -- Schematic: Wonderheal XT68 Shades
+    [15] = 35192, -- Schematic: Primal-Attuned Goggles
+    [16] = 35193, -- Schematic: Lightning Etched Specs
+    [17] = 35194, -- Schematic: Surestrike Goggles v3.0
+    [18] = 35195, -- Schematic: Mayhem Projection Goggles
+    [19] = 35196, -- Schematic: Hard Khorium Goggles
+    [20] = 35197, -- Schematic: Quad Deathblow X44 Goggles
+    [21] = 35210, -- Plans: Sunblessed Breastplate
+    [22] = 35211, -- Plans: Hard Khorium Battleplate
+
+
     -- Trash drops
-    [1] = 99999, -- TODO
+    [23] = 34351, -- Tranquil Majesty Wraps
+    [24] = 34407, -- Tranquil Moonlight Wraps
+    [25] = 34350, -- Gauntlets of the Ancient Shadowmooon
+    [26] = 34409, -- Gauntlets of the Ancient Frostwolf
+    [27] = 35733, -- Ring of Harmonic Beauty
+    [28] = 34183, -- Shivering Felspine
+    [29] = 34346, -- Mounting Vengeance
+    [30] = 34349, -- Blade of Life's Inevitability
+    [31] = 34348, -- Wand of Cleansing Light
+    [32] = 34347, -- Wand of the Demonsoul
+
+    -- Kalecgos
+    [33] = 34848, -- Bracers of the Forgotten Conqueror
+    [34] = 34851, -- Bracers of the Forgotten Protector
+    [35] = 34852, -- Bracers of the Forgotten Vanquisher
+    [36] = 34170, -- Pantaloons of Calming Strife
+    [37] = 34386, -- Pantaloons of Growing Strife
+    [38] = 34169, -- Breeches of Natural Aggression
+    [39] = 34384, -- Breeches of Natural Splendor
+    [40] = 34168, -- Starstalker Legguards
+    [41] = 34167, -- Legplates of the Holy Juggernaut
+    [42] = 34382, -- Judicator's Legguards
+    [43] = 34166, -- Band of Lucent Beams
+    [44] = 34165, -- Fang of Kalecgos
+    [45] = 34164, -- Dragonscale-Encrusted Longblade
+
+    -- Brutallus
+    [46] = 34853, -- Belt of the Forgotten Conqueror
+    [47] = 34854, -- Belt of the Forgotten Protector
+    [48] = 34855, -- Belt of the Forgotten Vanquisher
+    [49] = 34181, -- Leggings of Calamity
+    [50] = 34180, -- Felfury Legplates
+    [51] = 34381, -- Felstrength Legplates
+    [52] = 34178, -- Collar of the Pit Lord
+    [53] = 34177, -- Clutch of Demise
+    [54] = 34179, -- Heart of the Pit
+    [55] = 34176, -- Reign of Misery
+
+    -- Felmyst
+    [56] = 34856, -- Boots of the Forgotten Conqueror
+    [57] = 34857, -- Boots of the Forgotten Protector
+    [58] = 34858, -- Boots of the Forgotten Vanquisher
+    [59] = 34352, -- Borderland Fortress Grips
+    [60] = 34188, -- Leggings of the Immortal Night
+    [61] = 34385, -- Leggings of the Immortal Beast
+    [62] = 34186, -- Chain Links of the Tumultuous Storm
+    [63] = 34383, -- Kilt of Spiritual Reconstruction
+    [64] = 34184, -- Brooch of the Highborne
+    [65] = 34185, -- Sword Breaker's Bulwark
+    [66] = 34182, -- Grand Magister's Staff of Torrents
+
+    -- Eredar Twins
+    [67] = 34205, -- Shroud of Redeemed Souls
+    [68] = 34190, -- Crimson Paragon's Cover
+    [69] = 34210, -- Amice of the Convoker
+    [70] = 34202, -- Shawl of Wonderment
+    [71] = 34393, -- Shoulderpads of Knowledge's Pursuit
+    [72] = 34209, -- Spaulders of Reclamation
+    [73] = 34391, -- Spaulders of Devastation
+    [74] = 34195, -- Shoulderpads of Vehemence
+    [75] = 34392, -- Demontooth Shoulderpads
+    [76] = 34194, -- Mantle of the Golden Forest
+    [77] = 34208, -- Equilibrium Epaulets
+    [78] = 34390, -- Erupting Epaulets
+    [79] = 34192, -- Pauldrons of Perseverance
+    [80] = 34388, -- Pauldrons of Berserking
+    [81] = 34193, -- Spaulders of the Thalassian Savior
+    [82] = 34389, -- Spaulders of the Thalassian Defender
+    [83] = 35290, -- Sin'dorei Pendant of Conquest
+    [84] = 35291, -- Sin'dorei Pendant of Salvation
+    [85] = 35292, -- Sin'dorei Pendant of Triumph
+    [86] = 34204, -- Amulet of Unfettered Magics
+    [87] = 34189, -- Band of Ruinous Delight
+    [88] = 34206, -- Book of Highborne Hymns
+    [88] = 34197, -- Shiv of Exsanguination
+    [89] = 34199, -- Archon's Gavel
+    [90] = 34203, -- Grip of Mannoroth
+    [91] = 34198, -- Stanchion of Primal Instinct
+    [92] = 34196, -- Golden Bow of Quel'Thalas
+
+    -- M'uru
+    [93] = 34232, -- Fel Conqueror Raiments
+    [94] = 34233, -- Robes of Faltered Light
+    [95] = 34399, -- Robes of Ghostly Hatred
+    [96] = 34212, -- Sunglow Vest
+    [97] = 34398, -- Utopian Tunic of Elune
+    [98] = 34211, -- Harness of Carnal Instinct
+    [99] = 34397, -- Bladed Chaos Tunic
+    [100] = 34234, -- Shadowed Gauntlets of Paroxysm
+    [101] = 34408, -- Gloves of the Forest Drifter
+    [102] = 34229, -- Garments of Serene Shores
+    [103] = 34396, -- Garments of Crashing Shores
+    [104] = 34228, -- Vicious Hawkstrider Hauberk
+    [105] = 34215, -- Warharness of Reckless Fury
+    [106] = 34394, -- Breastplate of Agony's Aversion
+    [107] = 34240, -- Gauntlets of the Soothed Soul
+    [108] = 34216, -- Heroic Judicator's Chestguard
+    [109] = 34395, -- Noble Judicator's Chestguard
+    [110] = 34213, -- Ring of Hardened Resolve
+    [111] = 34230, -- Ring of Omnipotence
+    [112] = 35282, -- Sin'dorei Band of Dominance
+    [113] = 35283, -- Sin'dorei Band of Salvation
+    [114] = 35284, -- Sin'dorei Band of Triumph
+    [115] = 34427, -- Blackened Naaru Sliver
+    [116] = 34430, -- Glimmering Naaru Sliver
+    [117] = 34429, -- Shifting Naaru Sliver
+    [118] = 34428, -- Steely Naaru Sliver
+    [119] = 34214, -- Murumasa
+    [120] = 34231, -- Aegis of Angelic Fortune
+
+    -- Kil'jaeden
+    [121] = 34334, -- Thori'dal, the Stars' Fury
+    [122] = 34241, -- Cloak of Unforgivable Sin
+    [123] = 34242, -- Tattered Cape of Antonidas
+    [124] = 34339, -- Cowl of Light's Purity
+    [125] = 34405, -- Helm of Arcane Purity
+    [126] = 34340, -- Dark Conjuror's Collar
+    [127] = 34342, -- Handguards of the Dawn
+    [128] = 34406, -- Gloves of Tyri's Power
+    [129] = 34344, -- Handguards of Defiled Worlds
+    [130] = 34244, -- Duplicitous Guise
+    [131] = 34404, -- Mask of the Fury Hunter
+    [132] = 34245, -- Cover of Ursol the Wise
+    [133] = 34403, -- Cover of Ursoc the Mighty
+    [134] = 34333, -- Coif of Alleria
+    [135] = 34332, -- Cowl of Gul'dan
+    [136] = 34402, -- Shroud of Chieftain Ner'zhul
+    [137] = 34343, -- Thalassian Ranger Gaunatlets
+    [138] = 34243, -- Helm of Burning Righteousness
+    [139] = 34401, -- Helm of Uther's Resolve
+    [140] = 34345, -- Crown of Anasterian
+    [141] = 34400, -- Crown of Dath'Remar
+    [142] = 34341, -- Borderland Paingrips
+    [143] = 34329, -- Crux of the Apocalypse
+    [144] = 34247, -- Apolyon, the Soul-Render
+    [145] = 34335, -- Hammer of Sanctification
+    [146] = 34331, -- Hand of the Deceiver
+    [147] = 34336, -- Sunflare
+    [148] = 34337, -- Golden Staff of the Sin'dorei
 }

16987563e30a9239c3ebca66c2b4d1bc7e7182f5
Mcaruano <mjcengineer89@yahoo.com>
2022-04-18 18:13:36 -0700

    Added initial DKP values for T6.5

--- TnTDKP/TierSixPointFiveBossConfig.lua
+++ TnTDKP/TierSixPointFiveBossConfig.lua
@@ -1,6 +1,23 @@
 -- The configuration which hosts boss-specific configuration variables.
 tierSixPointFiveBossConfig = {	
-	["TODO"] = {
-		["KillDKPAward"] = 100,
+	["Kalecgos"] = {
+		["KillDKPAward"] = 1570,
 	},
+	["Brutallus"] = {
+		["KillDKPAward"] = 1570,
+	},
+	["Felmyst"] = {
+		["KillDKPAward"] = 1570,
+	},
+
+	-- Can redistribute the DKP later if I want to
+	-- ["Grand Warlock Alythess"] = {
+	-- 	["KillDKPAward"] = 785,
+	-- },
+	-- ["Entropius"] = {
+	-- 	["KillDKPAward"] = 785,
+	-- },
+	-- ["Kil'jaeden"] = {
+	-- 	["KillDKPAward"] = 785,
+	-- },
 }

a4847d33d435317c94b03c121f1ff5b5ade06bbc
Matt Caruano <mjcengineer89@yahoo.com>
2022-05-10 22:24:35 -0700

    Removing DKP awards for Vashj and KT so the AddOn doesn't spam if you do Magister's Terrace with teh AddOn enabled

--- TnTDKP/TierFiveBossConfig.lua
+++ TnTDKP/TierFiveBossConfig.lua
@@ -1,10 +1,10 @@
 -- The configuration which hosts boss-specific configuration variables.
 tierFiveBossConfig = {	
-	["Lady Vashj"] = {
-		["KillDKPAward"] = 590,
-	},
+	-- ["Lady Vashj"] = {
+	-- 	["KillDKPAward"] = 590,
+	-- },
 
-	["Kael'thas Sunstrider"] = {
-		["KillDKPAward"] = 590,
-	},
+	-- ["Kael'thas Sunstrider"] = {
+	-- 	["KillDKPAward"] = 590,
+	-- },
 }
